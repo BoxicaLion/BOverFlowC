cc9643e2c463bf5057df0d3ffc5706d612b7c5d2
Radu Enachi <radu.enachi@gmail.com>
2020-03-23 15:49:32 -0700

    This is the first commit

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@
 
 
 int main() {
-    int n = INT_MAX -2;
+    int n = INT_MAX -2; // checking whether the C++ is counting the "V bite" or is ignoring;
 
     for(int i = 0 ; i < 6; i++){
         std::printf(" n == %d\n", n);
         n++;
     }
+
+
     return 0;
 }
